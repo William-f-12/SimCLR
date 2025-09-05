2b461b16f0b397c13f812573e41180ce3e7f9cf1
X <77029666+William-f-12@users.noreply.github.com>
2025-09-04 23:22:26 -0500

    modify data_aug

--- src/data_aug.py
+++ src/data_aug.py
@@ -1,11 +1,9 @@
-import random
 import torchvision.transforms as T
 
 
 class DataAugmentation:
     def __init__(self, img_size, transform = None):
         self.img_size = img_size
-        self.color_jitter = T.ColorJitter(0.8, 0.8, 0.8, 0.2)
         self.transform = self._random_compose(transform)
 
     def _random_compose(self, transform):
@@ -13,22 +11,15 @@ class DataAugmentation:
         if transform is not None:
             return transform
 
-        if random.random() < 0.7:
-            return T.Compose([
-                T.RandomResizedCrop(size=self.img_size, scale=(0.2, 1.0)),
-                T.RandomRotation(degrees = 180),
-                T.RandomApply([self.color_jitter], p=0.8),
-                T.GaussianBlur(kernel_size=int(0.1 * self.img_size) * 2 + 1, sigma=(0.1, 2.0)),
-                T.ToTensor()
-                ])
-        else:
-            return T.Compose([
-                T.RandomResizedCrop(size=self.img_size, scale=(0.2, 1.0)),
-                T.RandomHorizontalFlip(),
-                T.RandomGrayscale(p=0.2),
-                T.GaussianBlur(kernel_size=int(0.1 * self.img_size) * 2 + 1, sigma=(0.1, 2.0)),
-                T.ToTensor()
-                ])
+        return T.Compose([
+            T.RandomResizedCrop(size=self.img_size, scale=(0.2, 1.0)),
+            T.RandomChoice([T.RandomRotation(degrees=90), 
+                            T.RandomVerticalFlip()],
+                            T.RandomHorizontalFlip(), p=[0.4, 0.3, 0.3]),
+            T.RandomChoice([T.ColorJitter(0.8, 0.8, 0.8, 0.2), 
+                            T.RandomGrayscale(p=0.6)], p=[0.5, 0.5]),
+            T.RandomApply([T.GaussianBlur(kernel_size=min(21, int(0.1 * self.img_size)*2+1), sigma=(0.1, 2.0))], p=0.5),
+            T.ToTensor()])
 
     def __call__(self, x):
         return self.transform(x), self.transform(x)
